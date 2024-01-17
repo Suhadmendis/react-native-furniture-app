a777dbd5d862253e0c1ce75c2ae1b05ba48628e2
Suhadmendis <suhad.a.mendis@gmail.com>
2024-01-18 01:29:55 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StyleSheet, StatusBar, Text, View, SafeAreaView } from "react-native";
+
+import Screen1 from "./src/components/screens/screen1";
+import Screen2 from "./src/components/screens/screen2";
+
+export default function App() {
+  return (
+    <Screen2 />
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
